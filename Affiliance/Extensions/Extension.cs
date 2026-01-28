b2fdadbe5f3bd155569b7b75194b30c5620aeb5e
zeyad12-d <zeadyasser054@gmail.com>
2026-01-28 13:16:35 +0200

    add_configration

---
+++ Affiliance/Extensions/Extension.cs
@@ -0,0 +1,10 @@
+﻿namespace Affiliance_Api.Extensions
+{
+    public static  class Extension
+    {
+        public static IServiceCollection AddSqlServier(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
