320a9b3e0d929d4e1d35ca38ad526c6d9a62f2b7
zeyad12-d <zeadyasser054@gmail.com>
2026-01-28 17:20:31 +0200

    add-Generic RepoPattern pattern

---
+++ Affiliance_Infrasturcture/Repostiory/GenericRepository.cs
@@ -0,0 +1,88 @@
+﻿using Affiliance_core.Interfaces;
+using Affiliance_Infrasturcture.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Affiliance_Infrasturcture.Repostiory
+{
+    class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AffiliancesDBcontext _context;
+
+        public GenericRepository(AffiliancesDBcontext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, string[]? includes = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return await query.Where(predicate).ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetPagedAsync(
+            int pageIndex,
+            int pageSize,
+            Expression<Func<T, bool>>? predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string[]? includes = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (predicate != null)
+                query = query.Where(predicate);
+
+            if (includes != null)
+                foreach (var include in includes) query = query.Include(include);
+
+            if (orderBy != null)
+                query = orderBy(query);
+
+            return await query
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+    }
+}
+
---
+++ Affiliance_core/interfaces/IGenericRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+
+namespace Affiliance_core.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+
+      
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+      
+        Task<IEnumerable<T>> FindAsync(
+            Expression<Func<T, bool>> predicate,
+            string[]? includes = null);
+
+        
+        Task<IEnumerable<T>> GetPagedAsync(
+            int pageIndex,
+            int pageSize,
+            Expression<Func<T, bool>>? predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string[]? includes = null);
+
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
