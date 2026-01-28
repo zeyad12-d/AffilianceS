5ff11782d08424a0d446c7cb8cc8f2ab40624615
zeyad12-d <zeadyasser054@gmail.com>
2026-01-28 16:18:44 +0200

    add-Extension >SQL -Identity-cors-JWT

--- Affiliance/Extensions/Extension.cs
+++ Affiliance/Extensions/Extension.cs
@@ -1,10 +1,122 @@
-﻿namespace Affiliance_Api.Extensions
+﻿using Affiliance_core.Entites;
+using Affiliance_Infrasturcture.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace Affiliance_Api.Extensions
 {
-    public static  class Extension
+    public static class Extensions
     {
-        public static IServiceCollection AddSqlServier(this IServiceCollection services, IConfiguration configuration)
+        #region AddSql
+        public static void AddSqlConnection(this IServiceCollection services, IConfiguration config)
         {
-            return services;
+            services.AddDbContext<AffiliancesDBcontext>(options =>
+                options.UseSqlServer(config.GetConnectionString("cs")));
         }
+        #endregion
+
+        #region Identity
+        public static void AddIdentityConfiguration(this IServiceCollection services)
+        {
+            // لاحظ استخدام <User, IdentityRole<int>> عشان الـ IDs
+            services.AddIdentity<User, IdentityRole<int>>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<AffiliancesDBcontext>()
+            .AddDefaultTokenProviders();
+        }
+        #endregion
+
+        #region Swagger
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Affiliance API",
+                    Version = "v1",
+                    Description = "API documentation for the Affiliate Platform project."
+                });
+
+            
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Enter 'Bearer' [space] and then your valid JWT token.",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+        }
+        #endregion
+
+        #region JWT
+        public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration config)
+        {
+            var jwtSettings = config.GetSection("JwtSettings");
+            var secretKey = jwtSettings["SecretKey"];
+            var issuer = jwtSettings["Issuer"];
+            var audience = jwtSettings["Audience"];
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+        }
+        #endregion
+
+        #region Cors
+        public static void AddCorsConfiguration(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+        }
+        #endregion
     }
 }
