aa9222222ac70f0199404c0314f85581c7e80f2f
zeyad12-d <zeadyasser054@gmail.com>
2026-01-29 15:28:45 +0200

    add- apiHelper

---
+++ Affiliance_core/ApiHelper/PagedResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Affiliance_core.ApiHelper
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Data { get; }
+
+        public int Page {  get;  }
+        public int PageSize {  get; }
+        public int TotalCount { get; }
+
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+        public PagedResult(IEnumerable<T> data , int page , int pagesize , int totalCount )
+        {
+            Data = data;
+            Page = page;
+            PageSize = pagesize;
+           TotalCount = totalCount;
+            
+        }
+    }
+}
