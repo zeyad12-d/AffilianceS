7234c401252d1e4f5d38ae4c83a04be6381ea927
zeyad12-d <zeadyasser054@gmail.com>
2026-02-08 15:26:28 +0200

    uodate-Service Manager

--- Affiliance_Infrasturcture/Repostiory/ServicesManager.cs
+++ Affiliance_Infrasturcture/Repostiory/ServicesManager.cs
@@ -6,13 +6,14 @@ namespace Affiliance_Infrasturcture.Repostiory
     {
         private readonly IServiceFactory _serviceFactory;
         private IAiService _aiService;
-        private readonly Lazy< ICampanyServices> _campanyServices;
+        private readonly Lazy<ICampanyServices> _campanyServices;
+        private readonly Lazy<ICategoryService> _categoryService;
 
         public ServicesManager(IServiceFactory serviceFactory)
         {
             _serviceFactory = serviceFactory ?? throw new ArgumentNullException(nameof(serviceFactory));
             _campanyServices = new Lazy<ICampanyServices>(() => _serviceFactory.CreateService<ICampanyServices>());
-
+            _categoryService = new Lazy<ICategoryService>(() => _serviceFactory.CreateService<ICategoryService>());
         }
 
         public IAiService AiService
@@ -27,5 +28,6 @@ namespace Affiliance_Infrasturcture.Repostiory
             }
         }
         public ICampanyServices CampanyServices => _campanyServices.Value;
+        public ICategoryService CategoryService => _categoryService.Value;
     }
 }
