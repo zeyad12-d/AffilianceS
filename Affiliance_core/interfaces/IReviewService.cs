fb50acfc55af11c173938df8133ff6118ae60052
zeyad12-d <zeadyasser054@gmail.com>
2026-02-09 13:56:01 +0200

    create Review Service

---
+++ Affiliance_core/interfaces/IReviewService.cs
@@ -0,0 +1,14 @@
+using Affiliance_core.ApiHelper;
+using Affiliance_core.Dto.ReviewDto;
+
+namespace Affiliance_core.interfaces
+{
+    public interface IReviewService
+    {
+        Task<ApiResponse<PagedResult<ReviewDto>>> GetReviewsAsync(int reviewedId, ReviewFilterDto? filter = null);
+        Task<ApiResponse<PagedResult<ReviewDto>>> GetReviewsGivenAsync(int reviewerId, int page = 1, int pageSize = 10);
+        Task<ApiResponse<AverageRatingDto>> GetAverageRatingAsync(int reviewedId);
+        Task<ApiResponse<ReviewDto>> CreateReviewAsync(int reviewerId, int reviewedId, int? campaignId, byte rating, string? comment);
+        Task<ApiResponse<bool>> DeleteReviewAsync(int reviewId, int reviewerId);
+    }
+}
