b5644e09b33c3a02e28cae06e176407f4cc02475
zeyad12-d <zeadyasser054@gmail.com>
2026-02-11 13:44:29 +0200

    add-RecentActivity Dto

---
+++ Affiliance_core/Dto/Shared/RecentActivityDto.cs
@@ -0,0 +1,9 @@
+namespace Affiliance_core.Dto.Shared
+{
+    public class RecentActivityDto
+    {
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
