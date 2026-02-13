2cdc72e5d23164177fd5d877d32214dc8100fe56
zeyad12-d <zeadyasser054@gmail.com>
2026-02-13 18:36:10 +0200

    Add -Company Action Dto

---
+++ Affiliance_core/Dto/CampanyDto/CompanyActionDto.cs
@@ -0,0 +1,11 @@
+namespace Affiliance_core.Dto.CampanyDto
+{
+    /// <summary>
+    /// CompanyActionDto - For admin approval/rejection actions
+    /// </summary>
+    public class CompanyActionDto
+    {
+        public string? Note { get; set; }
+        public string? ResponseNotes { get; set; }
+    }
+}
