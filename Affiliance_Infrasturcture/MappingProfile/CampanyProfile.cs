17e5425caf4f6efd56872d089a4074f09465a1eb
zeyad12-d <zeadyasser054@gmail.com>
2026-02-15 00:30:08 +0200

    update_company profile

--- Affiliance_Infrasturcture/MappingProfile/CampanyProfile.cs
+++ Affiliance_Infrasturcture/MappingProfile/CampanyProfile.cs
@@ -1,13 +1,15 @@
 ﻿using Affiliance_core.Dto.AccountDto;
+using Affiliance_core.Dto.CampanyDto;
 using Affiliance_core.Entites;
+using AutoMapper;
 
 namespace Affiliance_Infrasturcture.MappingProfile
 {
-    public class CampanyProfile:AutoMapper.Profile
+    public class CampanyProfile : Profile
     {
-
         public CampanyProfile()
         {
+            #region Registration Mapping
             CreateMap<CompanyRegisterDto, Company>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.UserId, opt => opt.Ignore())
@@ -24,7 +26,36 @@ namespace Affiliance_Infrasturcture.MappingProfile
                 .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
                 .ForMember(dest => dest.Website, opt => opt.MapFrom(src => src.Website))
                 .ForMember(dest => dest.TaxId, opt => opt.MapFrom(src => src.TaxId));
+            #endregion
 
+            #region Company to DTO Mappings
+            // Company to CompanyDto
+            CreateMap<Company, CompanyDto>()
+                .ForMember(dest => dest.LogoUrl, opt => opt.MapFrom(src => src.LogoUrl))
+                .ForMember(dest => dest.IsVerified, opt => opt.MapFrom(src => src.IsVerified))
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => src.UpdatedAt))
+                .ReverseMap();
+
+            // Company to CompanyDetailsDto
+            CreateMap<Company, CompanyDetailsDto>()
+                .ForMember(dest => dest.LogoUrl, opt => opt.MapFrom(src => src.LogoUrl))
+                .ForMember(dest => dest.IsVerified, opt => opt.MapFrom(src => src.IsVerified))
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => src.UpdatedAt))
+                .ForMember(dest => dest.UserEmail, opt => opt.MapFrom(src => src.User != null ? src.User.Email : null));
+
+
+            // UpdateCompanyDto to Company
+            CreateMap<UpdateCompanyDto, Company>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            // Company to CompanyApprovalDto
+            CreateMap<Company, CompanyApprovalDto>()
+                .ForMember(dest => dest.UserEmail, opt => opt.MapFrom(src => src.User != null ? src.User.Email : null))
+                .ForMember(dest => dest.LogoUrl, opt => opt.MapFrom(src => src.LogoUrl))
+                .ForMember(dest => dest.IsVerified, opt => opt.MapFrom(src => src.IsVerified));
+            #endregion
         }
     }
 }
